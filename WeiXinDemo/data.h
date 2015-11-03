5a4f9af78faafc495d30f3e3e7ca4e48d6c5821a
李双龙 <1462113660@qq.com>
2015-11-03 19:58:40 +0800

    11
    11

---
+++ WeiXinDemo/data.h
@@ -0,0 +1,13 @@
+//
+//  data.h
+//  WeiXinDemo
+//
+//  Created by 中软国际519 on 15/11/3.
+//  Copyright (c) 2015年 nate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface data : NSObject
+
+@end
---
+++ WeiXinDemo/data.m
@@ -0,0 +1,13 @@
+//
+//  data.m
+//  WeiXinDemo
+//
+//  Created by 中软国际519 on 15/11/3.
+//  Copyright (c) 2015年 nate. All rights reserved.
+//
+
+#import "data.h"
+
+@implementation data
+
+@end
